fcdb263cfe41b07f34c748776c12d6f7fd968dd1
immortalkingkev <kevindamier@hotmail.com>
2015-12-10 22:13:23 +0000

    added images and command functions.

--- basketball.js
+++ basketball.js
@@ -1,3 +1,11 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
+   
+   if(name.length>15){
+       alert("Wow, that's a long name!")
+   }
+    return name;
 };
+var tryTwoPointShot = function (){
+    
+}
